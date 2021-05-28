780cdde63e68e7190bcd2280177229c52efc0132
picklejar76 <picklejar76@gmail.com>
2021-05-28 14:07:38 -0500

    added json data for 7 infocom games

---
+++ zilf-forked/zil2json.cmd
@@ -0,0 +1,7 @@
+Zilf.exe -c C:\opt\zork-playground\historicalsource\zork1\zork1.zil C:\opt\zork-playground\zil-to-json\data\zork1\zork1.zap
+Zilf.exe -c C:\opt\zork-playground\historicalsource\zork2\zork2.zil C:\opt\zork-playground\zil-to-json\data\zork2\zork2.zap
+Zilf.exe -c C:\opt\zork-playground\historicalsource\zork3\zork3.zil C:\opt\zork-playground\zil-to-json\data\zork3\zork3.zap
+Zilf.exe -c C:\opt\zork-playground\historicalsource\enchanter\enchanter.zil C:\opt\zork-playground\zil-to-json\data\enchanter\enchanter.zap
+Zilf.exe -c C:\opt\zork-playground\historicalsource\sorcerer\sorcerer.zil C:\opt\zork-playground\zil-to-json\data\sorcerer\sorcerer.zap
+Zilf.exe -c C:\opt\zork-playground\historicalsource\spellbreaker\z6.zil C:\opt\zork-playground\zil-to-json\data\spellbreaker\spellbreaker.zap
+Zilf.exe -c C:\opt\zork-playground\historicalsource\wishbringer\wishbringer.zil C:\opt\zork-playground\zil-to-json\data\wishbringer\wishbringer.zap
